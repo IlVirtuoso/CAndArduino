d2314eb915f4545f508eca8d2788cec1370c7682
IlVirtuoso <matteo.ielacqua@edu.unito.it>
2019-11-10 17:31:19 +0100

    wsl

--- csvcalc.c
+++ csvcalc.c
@@ -1,45 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
 
 FILE * csv;
+
+
+
+
 
 int main(int argc, char * argv[]){
     csv = fopen("./PPAY_ListaMovimenti.CSV", "r");
     fseek(csv,0,0);
-    char *** matrix = (char***) malloc(sizeof(char**)*sizeof(char*)*sizeof(char***));
-    char c;
-    int i = 0;
-    int j = 0;
-    int buf = 0;
-    char buffer[40];
-    while((c = fgetc(csv)) != EOF){
-        if(c == ';'){
-            for(int b = 0; b < buf; b++){
-                matrix[i][j][b] = buffer[b];
-                printf("%c",matrix[i][j][b]);
-            }
-            buf = 0;
-            j++;
-        }
-        else if(c == '\n'){
-            i++;
-            j = 0;
-            
-        }
-        else{
-            buffer[buf] = c;
-            buf++;
-            
-        }
+    char *** matrix = (char***)malloc(sizeof(entrySize(csv) * sizeof(char***)));
+    int r = 0; //Row
+    int c = 0; //Column
+    int b = 0; //Buffer
+    matrix[r] = (char*)malloc(sizeof(char*));
+    matrix[r][c] = (char**)malloc(sizeof(char**));
+    while((matrix[r][c][b] = getc(csv)) != EOF){
+        
     }
-    for(int i = 0; i < sizeof(matrix);i++){
-        for(int j = 0; j < sizeof(matrix[i]);j++){
-            for(int b = 0; b < sizeof(matrix[i][j]); b++){
-                printf("%c",matrix[i][j][b]);
-            }
-        }
-    }
+    
+    
     
 }
 
