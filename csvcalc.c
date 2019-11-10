cf1c1c863ae48b553c6d1e15538b70e7ced850f6
IlVirtuoso <matteo.ielacqua@edu.unito.it>
2019-11-10 21:23:35 +0100

    seg fault

--- csvcalc.c
+++ csvcalc.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
 
 FILE * csv;
 
@@ -10,19 +12,42 @@ FILE * csv;
 
 int main(int argc, char * argv[]){
     csv = fopen("./PPAY_ListaMovimenti.CSV", "r");
-    fseek(csv,0,0);
-    char *** matrix = (char***)malloc(sizeof(entrySize(csv) * sizeof(char***)));
+    char matrix[countLines(csv)][countEntries(csv)][200];
     int r = 0; //Row
     int c = 0; //Column
     int b = 0; //Buffer
-    matrix[r] = (char*)malloc(sizeof(char*));
-    matrix[r][c] = (char**)malloc(sizeof(char**));
-    while((matrix[r][c][b] = getc(csv)) != EOF){
+    printf("Entries are: %d",countEntries(csv));
+    char buf;
+    fseek(csv,0,0);
+    while ((buf = fgetc(csv)) != EOF)
+    {
+        switch (buf)
+        {
+        case ';':
+        buf = ' ';
+        c++;
+        b = 0;
+        break;
+
+        case ' ':
+
+        break;
+
+        case '\n':
+        buf = ' ';
+        r++;
+        c = 0;
         
+        break;
+        
+        default:
+        printf("Writing on: Row: %d, Column: %d, Buffer: %d, Content: %c \n",r,c,b,buf);
+        matrix[r][c][b] = buf;
+        b++;
+            break;
+        }
     }
-    
-    
-    
+    print(matrix,r,c);
 }
 
 int countLines(FILE * buffer){
@@ -35,6 +60,7 @@ int countLines(FILE * buffer){
             lines++;
         }
     }
+    return lines;
     
 }
 
@@ -49,3 +75,51 @@ int entrySize(FILE * buffer){
     }
     return size;
 }
+
+int countEntries(FILE * buffer){
+    int entries = 0;
+    fseek(buffer,0,0);
+    char c;
+    while((c = fgetc(buffer)) != '\n'){
+     if(c == ';'){
+         entries++;
+         c = ' ';
+     } 
+    }
+    return entries;
+}
+
+void parse(FILE * buffer){
+    fseek(buffer,0,0);
+    char c;
+    while((c = fgetc(buffer)) != EOF){
+        switch (c)
+        {
+
+        case '\n':
+        c = ' ';
+            printf("  [return] \n");
+            break;
+
+        case ';':
+        printf("  [Entry];");
+        c = ' ';
+        break;
+        
+        default:
+        printf("%c",c);
+        c = ' ';
+            break;
+        }
+        
+    }
+}
+
+void print(char *(*(*matrix)),int rows, int columns){
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < columns; j++){
+            printf("|%s|",matrix[i][j]);
+        }
+    }
+}
+
