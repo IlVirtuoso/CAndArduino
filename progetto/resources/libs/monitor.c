cdc52248edd3f3fcf10a84ef91a5ae666ac02550
HPF4 <SbX?1398>
2020-01-15 17:06:59 +0100

    Cambiamento radicale al display (da provare)

--- progetto/resources/libs/monitor.c
+++ progetto/resources/libs/monitor.c
@@ -3,45 +3,23 @@
 #include "monitor.h"
 #endif
 
-int x;
-int y;
 void display(cell *shared_table)
 {
-    for (x = 0; x < SO_ALTEZZA; x++)
-    {
-        if (x == 0)
-        {
-            for (y = 0; y < SO_BASE; y++)
-            {
-                if (y == 0)
-                {
-                    if (SO_ALTEZZA > 10)
-                        printf("---%d-", y);
-                    else
-                        printf("-%d-", y);
-                }
-                else
-                    printf("%d-", y % 10);
-            }
-            printf("\n");
-        }
-        for (y = 0; y < SO_BASE; y++)
-        {
-            if (y == 0)
-            {
-
-                if (SO_ALTEZZA > 10 && x < 10)
-                    printf("%d |%c|", x, getid(shared_table, x, y));
-                else if (SO_ALTEZZA <= 10 || x > 9)
-                    printf("%d|%c|", x, getid(shared_table, x, y));
-            }
-            else
-            {
-                printf("%c|", getid(shared_table, x, y));
-            }
+    int x, y, i, j;
+    for(i = 0; i < SO_ALTEZZA; i++){
+        if(i<10) printf(" 0%d",i);
+        else printf(" %d",i);
+    }
+    printf(\n);
+    for(i = 0; i < SO_BASE; i++){
+        if(i<10) printf("0%d",i);
+        else printf("%d",i);
+        for(j = 0; j < SO_ALTEZZA; j++){
+            printf(" %c", getid(shared_table,x,y));
         }
         printf("\n");
     }
+    printf("\n");
 }
 
 void display_sem()
